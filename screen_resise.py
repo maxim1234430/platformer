de26692818310843cf32177db1fa15ff5f5eeb23
maxim1234430 <max.ponomarev2010@gmail.com>
2025-05-11 11:10:32 +0300

    11.05

--- screen_resise.py
+++ screen_resise.py
@@ -20,12 +20,28 @@ class Camera():
 
     # вычисляем центр игрока
     def player_center(self, target):
+        #сохраняем центр игрока в переменную
         center_x = int(target.centerx)
         center_y = int(target.centery)
 
+        #вычисляем следующую позицию игрока
+        next_position_x = -(center_x * self.zoom) + self.screen_width//2
+        next_position_y = -(center_y * self.zoom) + self.screen_height//2
 
-        self.offset_x = -(center_x * self.zoom) + self.screen_width//2
-        self.offset_y = -(center_y * self.zoom) + self.screen_height//2
+        #плавно перемещаем камеру к позиции
+        self.offset_x  += (next_position_x - self.offset_x ) * self.smoothness
+        self.offset_y += (next_position_y - self.offset_y) * self.smoothness
+
+        #округляем до целого значения
+        self.offset_x = int(self.offset_x )
+        self.offset_y = int(self.offset_y )
+
+
+        #не допускаем выход камеры за карту
+        if self.offset_x >0:
+            self.offset_x = 0
+        if self.offset_y >0:
+            self.offset_y = 0
 
 
 
